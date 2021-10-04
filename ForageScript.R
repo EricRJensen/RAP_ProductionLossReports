2f9991ae1fc48282a354ec764d16c9851c00e8af
Eric Jensen <33233973+moonlightbotanist@users.noreply.github.com>
2021-10-04 15:33:42 -0600

    Updated main landing pages to support biome specific access to the reports

--- ForageScript.R
+++ ForageScript.R
@@ -206,108 +206,108 @@ for(i in stas_fp_v){ #stas_fp_v
   #                                 rap_url = rap_sta_url,
   #                                 type = 'State'))
 
-      # for(j in sta_ctys_v){
-      # 
-      #   # Subset to county sf object and name
-      #   cty_sf <- filter(ctys_sf, COUNTYID == j) #dataframe with county names
-      # 
-      #   #Create county directory
-      #   cty_name_s <- str_replace_all(cty_sf$NAME, ' ', '')
-      #   cty_name_l <- cty_sf$NAMELSAD #county name
-      #   cty_dir <- paste(sta_dir, cty_name_s, '/', sep = '')
-      #   dir.create(cty_dir)
-      # 
-      #   # Location name as title parameter for RMD
-      #   loc_name <- paste(cty_name_l, ', ', sta_name, sep = '') #concatenated 'County, State' name
-      # 
-      #   # Filter SF objects for county of interest
-      #   cty_df <- ctys_df %>%
-      #     filter(COUNTYID == j) %>%
-      #     mutate(cumYieldGap = cumsum(yieldgap),
-      #            totalArea = countyArea) %>%
-      #     dplyr::select(-countyArea)
-      # 
-      #   cty_df_csv <- cty_df %>%
-      #     mutate(staName = sta_name) %>%
-      #     dplyr::select(c(ctyName = NAME, staName, totalArea, analysisArea, biomass, yieldGap = yieldgap, cumYieldGap, treeCover, treeArea, classWoodlands, classModCover, classLowCover, classAtRisk, classIntact))
-      # 
-      #   # Generate RAP url for county
-      #   coords <- st_coordinates(st_centroid(cty_sf))
-      #   rap_cty_url = paste('https://rangelands.app/rap/?biomass_t=herbaceous&ll=', coords[2], ',', coords[1], '&z=9&landcover_t=tree&landcover_v=true', sep = '')
-      # 
-      #   # Write out html table to add to report
-      #   data_html <- cty_df %>%
-      #     mutate(Year = round(year, 0),
-      #            `Production (tons)` = format(round(biomass, 0), big.mark=","),
-      #            `Production gain/loss (tons)` = ifelse(yieldgap >= 0, paste(format(round(-yieldgap, 0), big.mark=",")), str_replace_all(paste('+', format(round(-yieldgap, 0), big.mark=","), sep = ''), ' ', '')),
-      #            `Tree area (acres)` = format(round(treeArea, 0), big.mark=",")) %>%
-      #     dplyr::select(c(Year, `Production (tons)`, `Production gain/loss (tons)`, `Tree area (acres)`)) %>%
-      #     tableHTML(widths = c(100,150,150,150), rownames = FALSE) %>% 
-      #     add_css_row(css = list(c('background-color', 'text-align', 'height', 'font-family'), c('#f2f2f2', 'center', '30px', 'Open Sans')),
-      #                 rows = odd(1:(nrow(sta_df)+1))) %>%
-      #     add_css_row(css = list(c('background-color', 'text-align', 'height', 'font-family'), c('#FFFFFF', 'center', '30px', 'Open Sans')),
-      #                 rows = even(1:nrow(sta_df))) 
-      #   
-      #   write_tableHTML(data_html, paste(here(), '/', cty_dir, 'data.html', sep = ''))
-      #   
-      #   # Generate rmarkdown reports
-      #   cty_csv_path <- paste(here(), '/', sta_dir, cty_name_s, '/data.csv', sep = '')
-      #   write_csv(cty_df_csv, path = cty_csv_path)
-      #   rmarkdown::render("Scripts/ForageReports_html.Rmd",
-      #                     output_file = paste(here(), '/', sta_dir, cty_name_s, '/index', sep = ''),
-      #                     output_format = html_document(),
-      #                     params = list(new_title = loc_name,
-      #                                   main_df = cty_df,
-      #                                   cty_sf = cty_sf,
-      #                                   sta_rast = sta_rast,
-      #                                   sta_sf = sta_sf,
-      #                                   sta_ctys_sf = sta_ctys_sf,
-      #                                   current_year = current_year,
-      #                                   rap_url = rap_cty_url,
-      #                                   main_dir = cty_dir,
-      #                                   type = 'County'))
-      # 
-      # # rmarkdown::render("Scripts/ForageReports_pdf.Rmd",
-      # #                   output_file = paste(here(), '/', sta_dir, cty_name_s, '/index.pdf', sep = ''),
-      # #                   output_format = pdf_document(),
-      # #                   params = list(new_title = loc_name,
-      # #                                 main_df = cty_df,
-      # #                                 cty_sf = cty_sf,
-      # #                                 sta_sf = sta_sf,
-      # #                                 sta_ctys_sf = sta_ctys_sf,
-      # #                                 current_year = current_year,
-      # #                                 rap_url = rap_cty_url,
-      # #                                 type = 'County'))
-      # 
-      #   # Export supplementary maps for download
-      #   # Annual production plot
-      #   png(file = paste(here(), '/', sta_dir, cty_name_s, '/production.png', sep = ''), width=6, height=4, units="in", res=300)
-      #   prod_map <- ggplot()+
-      #     geom_line(cty_df, mapping = aes(x = year, y = biomass), color = 'grey20', size = 1)+
-      #     geom_point(cty_df, mapping = aes(x = year, y = biomass), color = 'grey20', size = 2)+
-      #     labs(title = paste('Herbaceous production for ', cty_name, ', ', sta_name, sep = ''), y = 'Annual production (tons)')+
-      #     scale_y_continuous(labels = comma, limits = c(0, (max(cty_df$biomass))*1.1))+
-      #     scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-      #     theme_minimal()+
-      #     theme(axis.title.x=element_blank(),
-      #           plot.title.position = "plot")
-      #   print(prod_map)
-      #   dev.off()
-      # 
-      #   # Tree cover area
-      #   png(file = paste(here(), '/', sta_dir, cty_name_s, '/tree.png', sep = ''), width=6, height=4, units="in", res=300)
-      #   tree_map <- ggplot()+
-      #     geom_line(cty_df, mapping = aes(x = year, y = treeArea), color = '#0E8E00', size = 1)+
-      #     geom_point(cty_df, mapping = aes(x = year, y = treeArea), color = '#0E8E00', size = 2)+
-      #     geom_hline(yintercept = head(cty_df$treeArea, n=1), linetype = 'dashed', color = 'grey30', size = 1)+
-      #     labs(title = paste('Tree cover for ', cty_name, ', ', sta_name, sep = ''), y = 'Tree cover (acres)')+
-      #     scale_y_continuous(labels = comma, limits = c(0, max(cty_df$treeArea)*1.1))+
-      #     scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-      #     theme_minimal()+
-      #     theme(axis.title.x=element_blank(),
-      #           plot.title.position = "plot")
-      #   print(tree_map)
-      #   dev.off()
-      #  }
+      for(j in sta_ctys_v){
+
+        # Subset to county sf object and name
+        cty_sf <- filter(ctys_sf, COUNTYID == j) #dataframe with county names
+
+        #Create county directory
+        cty_name_s <- str_replace_all(cty_sf$NAME, ' ', '')
+        cty_name_l <- cty_sf$NAMELSAD #county name
+        cty_dir <- paste(sta_dir, cty_name_s, '/', sep = '')
+        dir.create(cty_dir)
+
+        # Location name as title parameter for RMD
+        loc_name <- paste(cty_name_l, ', ', sta_name, sep = '') #concatenated 'County, State' name
+
+        # Filter SF objects for county of interest
+        cty_df <- ctys_df %>%
+          filter(COUNTYID == j) %>%
+          mutate(cumYieldGap = cumsum(yieldgap),
+                 totalArea = countyArea) %>%
+          dplyr::select(-countyArea)
+
+        cty_df_csv <- cty_df %>%
+          mutate(staName = sta_name) %>%
+          dplyr::select(c(ctyName = NAME, staName, totalArea, analysisArea, biomass, yieldGap = yieldgap, cumYieldGap, treeCover, treeArea, classWoodlands, classModCover, classLowCover, classAtRisk, classIntact))
+
+        # Generate RAP url for county
+        coords <- st_coordinates(st_centroid(cty_sf))
+        rap_cty_url = paste('https://rangelands.app/rap/?biomass_t=herbaceous&ll=', coords[2], ',', coords[1], '&z=9&landcover_t=tree&landcover_v=true', sep = '')
+
+        # Write out html table to add to report
+        data_html <- cty_df %>%
+          mutate(Year = round(year, 0),
+                 `Production (tons)` = format(round(biomass, 0), big.mark=","),
+                 `Production gain/loss (tons)` = ifelse(yieldgap >= 0, paste(format(round(-yieldgap, 0), big.mark=",")), str_replace_all(paste('+', format(round(-yieldgap, 0), big.mark=","), sep = ''), ' ', '')),
+                 `Tree area (acres)` = format(round(treeArea, 0), big.mark=",")) %>%
+          dplyr::select(c(Year, `Production (tons)`, `Production gain/loss (tons)`, `Tree area (acres)`)) %>%
+          tableHTML(widths = c(100,150,150,150), rownames = FALSE) %>%
+          add_css_row(css = list(c('background-color', 'text-align', 'height', 'font-family'), c('#f2f2f2', 'center', '30px', 'Open Sans')),
+                      rows = odd(1:(nrow(sta_df)+1))) %>%
+          add_css_row(css = list(c('background-color', 'text-align', 'height', 'font-family'), c('#FFFFFF', 'center', '30px', 'Open Sans')),
+                      rows = even(1:nrow(sta_df)))
+
+        write_tableHTML(data_html, paste(here(), '/', cty_dir, 'data.html', sep = ''))
+
+        # Generate rmarkdown reports
+        cty_csv_path <- paste(here(), '/', sta_dir, cty_name_s, '/data.csv', sep = '')
+        write_csv(cty_df_csv, path = cty_csv_path)
+        rmarkdown::render("Scripts/ForageReports_html.Rmd",
+                          output_file = paste(here(), '/', sta_dir, cty_name_s, '/index', sep = ''),
+                          output_format = html_document(),
+                          params = list(new_title = loc_name,
+                                        main_df = cty_df,
+                                        cty_sf = cty_sf,
+                                        sta_rast = sta_rast,
+                                        sta_sf = sta_sf,
+                                        sta_ctys_sf = sta_ctys_sf,
+                                        current_year = current_year,
+                                        rap_url = rap_cty_url,
+                                        main_dir = cty_dir,
+                                        type = 'County'))
+
+      # rmarkdown::render("Scripts/ForageReports_pdf.Rmd",
+      #                   output_file = paste(here(), '/', sta_dir, cty_name_s, '/index.pdf', sep = ''),
+      #                   output_format = pdf_document(),
+      #                   params = list(new_title = loc_name,
+      #                                 main_df = cty_df,
+      #                                 cty_sf = cty_sf,
+      #                                 sta_sf = sta_sf,
+      #                                 sta_ctys_sf = sta_ctys_sf,
+      #                                 current_year = current_year,
+      #                                 rap_url = rap_cty_url,
+      #                                 type = 'County'))
+
+        # Export supplementary maps for download
+        # Annual production plot
+        png(file = paste(here(), '/', sta_dir, cty_name_s, '/production.png', sep = ''), width=6, height=4, units="in", res=300)
+        prod_map <- ggplot()+
+          geom_line(cty_df, mapping = aes(x = year, y = biomass), color = 'grey20', size = 1)+
+          geom_point(cty_df, mapping = aes(x = year, y = biomass), color = 'grey20', size = 2)+
+          labs(title = paste('Herbaceous production for ', cty_name, ', ', sta_name, sep = ''), y = 'Annual production (tons)')+
+          scale_y_continuous(labels = comma, limits = c(0, (max(cty_df$biomass))*1.1))+
+          scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+          theme_minimal()+
+          theme(axis.title.x=element_blank(),
+                plot.title.position = "plot")
+        print(prod_map)
+        dev.off()
+
+        # Tree cover area
+        png(file = paste(here(), '/', sta_dir, cty_name_s, '/tree.png', sep = ''), width=6, height=4, units="in", res=300)
+        tree_map <- ggplot()+
+          geom_line(cty_df, mapping = aes(x = year, y = treeArea), color = '#0E8E00', size = 1)+
+          geom_point(cty_df, mapping = aes(x = year, y = treeArea), color = '#0E8E00', size = 2)+
+          geom_hline(yintercept = head(cty_df$treeArea, n=1), linetype = 'dashed', color = 'grey30', size = 1)+
+          labs(title = paste('Tree cover for ', cty_name, ', ', sta_name, sep = ''), y = 'Tree cover (acres)')+
+          scale_y_continuous(labels = comma, limits = c(0, max(cty_df$treeArea)*1.1))+
+          scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+          theme_minimal()+
+          theme(axis.title.x=element_blank(),
+                plot.title.position = "plot")
+        print(tree_map)
+        dev.off()
+       }
  }
 
--- Modal_AnalysisArea.md
+++
@@ -1,7 +0,0 @@
-These reports estimate herbaceous production changes for "less intensively modified areas" across the western US and, thus, exclude some land cover types from analysis.  
-
-The values in this table are:  
-
-* Total area: The total area of the state or county covered by the report.  
-* Analysis area: The land area used generate this report. This includes all land cover types (ex. shrublands, grasslands, forest, etc.) other than row crop agriculture and developed landscapes, which are excluded from the analysis using the National Land Cover Database (2016) (NLCD) and National Agricultural Statistics Service (NASS) Cropland Data Layers.
-* Area excluded from analysis: The land area removed from this analysis. Specifically, this is the total area classified as agricultural or developed by the NLCD and NASS datasets.
